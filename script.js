93b9e255c693cedf1648644a6fa1d0f6d2ca877b
Tarun sharma <tarun.sharmad35@gmail.com>
2023-08-01 15:36:32 +0530

    Wheather app using java-script

---
+++ script.js
@@ -0,0 +1,48 @@
+/**
+ * Weather App
+ */
+
+// API_KEY for maps api
+let API_KEY = "a8e71c9932b20c4ceb0aed183e6a83bb";
+
+/**
+ * Retrieve weather data from openweathermap
+ */
+const getWeatherData = (city) => {
+  const URL = "https://api.openweathermap.org/data/2.5/weather";
+  const FULL_URL = `${URL}?q=${city}&appid=${API_KEY}&units=imperial`;
+  return fetch(FULL_URL)
+  .then(Response=>{
+    return Response.json()
+  })
+}
+
+/**
+ * Retrieve city input and get the weather data
+ */
+const searchCity = () => {
+
+    document.getElementById('welcomeDiv').style.display = "block";
+
+  const city = document.getElementById('city-input').value;
+  getWeatherData(city)
+  .then((res)=>{
+    showWeatherData(res);
+  }).catch((error)=>{
+    console.log(error);
+    console.log("Something happend");
+  })
+
+}
+
+/**
+ * Show the weather data in HTML
+ */
+showWeatherData = (weatherData) => {
+  document.getElementById("city-name").innerText = weatherData.name;
+  document.getElementById("weather-type").innerText = weatherData.weather[0].main;
+  document.getElementById("temp").innerText = weatherData.main.temp;
+  document.getElementById("min-temp").innerText = weatherData.main.temp_min;
+  document.getElementById("max-temp").innerText = weatherData.main.temp_max;
+}
+
